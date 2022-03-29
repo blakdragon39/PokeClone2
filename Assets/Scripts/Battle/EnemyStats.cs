13606ec0fb7aff524473a6413b8c7d2af10de506
Blakdragon39 <jenna.hartley@live.ca>
2022-03-29 17:21:37 -0600

    Showing enemy type in battle

--- Assets/Scripts/Battle/EnemyStats.cs
+++ Assets/Scripts/Battle/EnemyStats.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class EnemyStats : MonoBehaviour {
+    
     public HPStats HPStats => hpStats;
     
     [SerializeField] private Text nameText;
+    [SerializeField] private Text typeText;
     [SerializeField] private HPStats hpStats;
 
     public void Setup(Enemy enemy) {
         nameText.text = enemy.Base.name;
+        typeText.text = enemy.Base.Type.ToString();
         hpStats.Setup(enemy.HP);
     }
 }
